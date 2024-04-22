7b38669221f781562ddef005cc3a94fac0bece6d
Alex Westerlund <alexwesterlund@outlook.com>
2024-04-22 18:12:03 -0400

    Adds high quality placeholder image

--- src/Body/FoodTruck/FoodTruck.js
+++ src/Body/FoodTruck/FoodTruck.js
@@ -1,16 +1,14 @@
-// FoodTruck.js
-
 import React from 'react';
+import './FoodTruck.css';
+//
+import comingSoon from './Images/comingSoon.png';
 
 function FoodTruck() {
-  return (
-    <div>
-      <h1>Food Truck Menu</h1>
-      <p>Experience the best of Ella's on the go with our food truck menu, featuring crowd favorites and new creations.</p>
-      <p>Perfect for a quick bite or a casual meal, our food truck brings the same quality and flavor as our main restaurant to a location near you.</p>
-      <p>Follow us on social media to find out where we'll be parked next!</p>
-    </div>
-  );
+    return (
+        <div className="foodTruck">
+            <img src={comingSoon} alt="" />
+        </div>
+    );
 }
 
 export default FoodTruck;
