d4ceaff54c57a98a5f18eb841067d297ace02e29
Alex Westerlund <alexwesterlund@outlook.com>
2024-04-23 14:20:00 -0400

    Outlines page sections

--- src/Body/FoodTruck/FoodTruck.js
+++ src/Body/FoodTruck/FoodTruck.js
@@ -7,6 +7,14 @@ function FoodTruck() {
     return (
         <div className="foodTruck">
             <img src={comingSoon} alt="" />
+
+            {/* Photo of the truck, background removed */}
+
+            {/* Some sort of cool headline */}
+
+            {/* Live calendar of upcoming locations */}
+
+            {/* A few photos of the truck at events, with some paragraphs or other market-y stuff */}
         </div>
     );
 }
