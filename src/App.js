53b3dfe05673accd9937f28085a2b39fbf89d92d
Alex Westerlund <alexwesterlund@outlook.com>
2024-05-29 21:37:48 -0400

    SHitty overhaul pre-launch for Brian's minimalism

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom'; // Add Routes and Route back in 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //
 // import Header from './Header/Header';
 //
-// import Home from './Body/Home';
+import Home from './Body/Home/Home';
 // import LunchMenu from './Body/Menu/LunchMenu';
 // import DinnerMenu from './Body/Menu/DinnerMenu';
 // import Drinks from './Body/Drinks/Drinks';
@@ -18,18 +18,16 @@ function App() {
         <Router>
             <div className="App">
                 {/* <Header /> */}
-                {/* <div className="bodyContent"> */}
-                    {/* <Routes> */}
-                        {/* <Route path="/" element={<DinnerMenu />} />  currently shows menu, but I can create an actual home page too. */}
+                <div className="bodyContent">
+                    <Routes>
+                        <Route path="/" element={<Home />} />  {/* currently shows menu, but I can create an actual home page too. */}
                         {/* <Route path="/lunchmenu" element={<LunchMenu />} /> */}
                         {/* <Route path="/dinnermenu" element={<DinnerMenu />} /> */}
                         {/* <Route path="/drinks" element={<Drinks />} /> */}
                         {/* <Route path="/foodtruck" element={<FoodTruck />} /> */}
-                    {/* </Routes> */}
-                {/* </div> */}
-                {/* <Footer /> */}
-
-                <h2>Ella's website coming soon!</h2>
+                    </Routes>
+                </div>
+                {/* <Footer /> */}                
             </div>
         </Router>
     );
--- src/Body/Home/Home.js
+++ src/Body/Home/Home.js
@@ -1,16 +1,41 @@
-// Home.js
-
 import React from 'react';
+import './Home.css'
+
+import Logo from './Images/ellasPNG.png'
+import IG from './Images/instagram.png'
+
 
 function Home() {
-  return (
-    <div>
-      <h1>Welcome to Ella's!</h1>
-      <p>Discover the finest dining experience with our exclusive dishes and serene ambiance. Join us for a journey of culinary delight.</p>
-      <p>Our chefs use only the freshest ingredients to craft meals that will leave you wanting more. From appetizers to desserts, every dish is a masterpiece.</p>
-      <p>Thank you for choosing Ella's. We look forward to serving you.</p>
-    </div>
-  );
+    return (
+        <div className="Home">
+            <div className="VStack">
+                <img src={Logo} alt="Restaurant logo" />    
+
+                <a href="Menu.pdf" className="menuPDF"><h3>See Menu</h3></a>
+
+                <div className="VStack">
+                    <br />
+                    <p>Chef-Driven Kitchen Serving Small Plates, Craft Sandwiches & NY Style Pizza. LOTS of Wine, Beer, and Full Bar. Tasting Events. Live Music.</p>
+                    <br />
+                    <p>Monday .................... Closed</p>
+                    <p>Tuesday .................. 12-8pm</p>
+                    <p>Wednesday ............ 12-8pm</p>
+                    <p>Thursday ................ 12-8pm</p>
+                    <p>Friday ................... 12-11pm</p>
+                    <p>Saturday ............... 12-11pm</p>
+                    <p>Sunday ................. 12-11pm</p>
+                    <br />
+                    <h1>Coming June 2024!</h1>
+                </div>
+
+                <a href="https://www.instagram.com/ellaskitchenandwinebar?igsh=bDJ6eTRzbWl5eWR6" className="HStack instagramLink">
+                    <img src={IG} alt="Link to Instagram Account" className="IGlogo" />
+                    <h3>@EllasKitchenAndWineBar</h3>
+                </a>
+            </div>
+
+        </div>
+    );
 }
 
 export default Home;
