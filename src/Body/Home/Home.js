b66fee5f22db673ec019cef3e6fb94bc0b2b7a58
Alex Westerlund <alexwesterlund@outlook.com>
2024-08-14 18:47:54 -0400

    New hours 8-14-2024

--- src/Body/Home/Home.js
+++ src/Body/Home/Home.js
@@ -28,8 +28,7 @@ function Home() {
                     <p>Thursday ................. 11am-9pm</p>
                     <p>Friday ...................... 11am-9pm</p>
                     <p>Saturday .................. 11am-9pm</p>
-                    <p>Sunday ......................... 10-4pm</p>
-                    <p>*Kitchen closes 1 hour earlier</p>
+                    <p>Sunday ......................... 12-7pm</p>
                     <br />
                 </div>
 
